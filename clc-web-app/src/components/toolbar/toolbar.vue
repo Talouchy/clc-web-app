dff36f089132e7aa96147635836fb20deca53782
Khushal <patelkhushal9033@gmail.com>
2019-01-06 14:11:45 +1100

    created toolbar component

---
+++ clc-web-app/src/components/toolbar/toolbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-toolbar app dark color="#ffffff">
+          <v-btn icon>
+            <v-icon color="#000000">arrow_back</v-icon>
+          </v-btn>
+        
+        <v-spacer></v-spacer>
+          <v-toolbar-title class="black--text">Dashboard</v-toolbar-title>
+
+          <v-spacer></v-spacer>
+
+          <v-btn icon>
+            <v-icon color="#000000">home</v-icon>
+          </v-btn>
+          </v-toolbar>  
+  
+</template>
+
+<script>
+
+
+  export default {
+    data: () => ({
+      drawer: null
+    }),
+    props: {
+      source: String
+    }
+  }
+</script>
