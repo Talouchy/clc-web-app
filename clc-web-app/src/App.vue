b52fcb131d5193437e29be89d13569784ed543c7
Talouchy <danyal.lakzian@gmail.com>
2019-01-06 12:43:03 +1100

    Simple changes

--- clc-web-app/src/App.vue
+++ clc-web-app/src/App.vue
@@ -15,19 +15,16 @@
       </v-btn>
     </v-toolbar>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   },
   data () {
     return {
