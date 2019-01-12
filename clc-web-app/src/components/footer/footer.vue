40cfd2e14cda4575f8089c47343e7170bb826369
Talouchy <danyal.lakzian@gmail.com>
2019-01-12 22:10:58 +1100

    Changed footer style

--- clc-web-app/src/components/footer/footer.vue
+++ clc-web-app/src/components/footer/footer.vue
@@ -1,33 +1,7 @@
 <template>
-  <v-footer
-    dark
-    height="auto"
-  >
-    <v-card
-      class="flex"
-      flat
-      tile
-    >
-      <v-card-title class="teal">
-        <strong class="subheading">Get connected with us on social networks!</strong>
-
-        <v-spacer></v-spacer>
-
-        <v-btn
-          v-for="icon in icons"
-          :key="icon"
-          class="mx-3"
-          dark
-          icon
-        >
-          <v-icon size="24px">{{ icon }}</v-icon>
-        </v-btn>
-      </v-card-title>
-
-      <v-card-actions class="grey darken-3 justify-center">
-        &copy;2018 — <strong>Vuetify</strong>
-      </v-card-actions>
-    </v-card>
+  <v-footer class="pa-3">
+    <v-spacer></v-spacer>
+    <div>&copy; KnD, {{ new Date().getFullYear() }}</div>
   </v-footer>
 </template>
 
@@ -36,11 +10,7 @@
   export default {
     data: () => ({
       icons: [
-        ' all_out',
-        ' all_out',
-        ' all_out',
-        ' all_out',
-        ' all_out'
+        'all_out',
       ]
     })
   }
