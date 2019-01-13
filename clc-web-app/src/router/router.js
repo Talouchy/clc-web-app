3f9d664977b14eae0ebb6c4f931b3a6e3982de2e
Talouchy <danyal.lakzian@gmail.com>
2019-01-13 16:10:47 +1100

    Updated the router with title pages

--- clc-web-app/src/router/router.js
+++ clc-web-app/src/router/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import HelloWorld from './../components/HelloWorld'
+import Welcome from './../components/Welcome'
 import Dashboard from './../components/Dashboard'
 
 Vue.use(VueRouter)
@@ -9,7 +9,17 @@ Vue.use(VueRouter)
 export default new VueRouter({
   routes: [
     // dynamic segments start with a colon
-    { path: '/', name:'helloworld', component: HelloWorld },
-    { path: '/home', name:'dashboard', component: Dashboard },
+    { 
+      path: '/',
+      name:'index',
+      pageTitle: 'Welcome to the CLC App',
+      component: Welcome,
+    },
+    {
+      path: '/dashboard',
+      name:'dashboard',
+      pageTitle: 'DASHBOARD',
+      component: Dashboard
+    },
   ]
 })
