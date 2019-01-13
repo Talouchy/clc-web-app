d44b6b2e50a6b08f3540d3220c1ac24f6e6e7432
Talouchy <danyal.lakzian@gmail.com>
2019-01-13 23:09:19 +1100

    Added feedback page link

--- clc-web-app/src/components/Welcome.vue
+++ clc-web-app/src/components/Welcome.vue
@@ -1,9 +1,6 @@
 <template>
   <v-container>
-    <v-layout
-      text-xs-center
-      wrap
-    >
+    <v-layout text-xs-center wrap>
       <v-flex mb-4>
         <h1 class="display-2 font-weight-bold mb-3">
           Welcome to CLC App
@@ -22,6 +19,11 @@
         <h2 class="headline font-weight-bold mb-3">
           <a href="/#/settings" class="subheading mx-3">
             Settings page
+          </a>
+        </h2>
+        <h2 class="headline font-weight-bold mb-3">
+          <a href="/#/feedback" class="subheading mx-3">
+            Feedback page
           </a>
         </h2>
       </v-flex>
