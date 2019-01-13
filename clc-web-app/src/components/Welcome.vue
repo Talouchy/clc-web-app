2d3ab641ba9081e68cfe445bc3d60ce89e3ce6e7
Talouchy <danyal.lakzian@gmail.com>
2019-01-13 16:09:09 +1100

    Updated welcome page

---
+++ clc-web-app/src/components/Welcome.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to CLC App
+        </h1>
+        <p class="subheading font-weight-regular">
+          This project is done by KnD
+        </p>
+      </v-flex>
+
+      <v-flex mb-5 xs12>
+        <h2 class="headline font-weight-bold mb-3">
+          <a href="http://localhost:8080/#/dashboard" class="subheading mx-3">
+            Start the CLC App
+          </a>
+        </h2>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+    })
+  }
+</script>
+
+<style>
+
+</style>
